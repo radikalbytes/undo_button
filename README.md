cb6b926e4dad71983da2060bf5de6e4e639e2e34
droky <radikalbytes@gmail.com>
2020-05-01 00:51:55 +0200

    Library Trinket keyboard and README

---
+++ README.md
@@ -0,0 +1,65 @@
+#UNDO Big Button
+![image1](./images/IMG_6554.JPG)
+Hello there!
+
+This is a fun project made at Covid-19 confinament from trash. 
+Sometimes we use CTRL+Z or CMD+Z to UNDO.
+
+If you use a lot of Undo shortcuts maybe your software skills are bad or you need put more attention at your work.
+
+This Big Undo Button starts in green color and goes to red every time you push it.
+
+You can feel how satisfactory is push it and obtain an Undo command... you can strike it, hugh it, push it, punch it... you'll love it, but every time you push it, Undo Button change more red and you will feel a noob.
+
+If you don't push it for a while ( 1 minute ), Undo Button shifts color less red and every minute returns to green.
+
+ 
+
+It's simple:
+
+* A big button 3D printed at home
+* Attiny85 PCB (Digispark clone)
+* 2xWS2812 RGB led
+* Microswitch
+* Old spring
+* Some double side tape
+
+##Electronics
+
+![image1](./images/IMG_6423.JPG)
+The WS2812 are conected serially (Attiny_Pin0 -> Din1 -> Dout1 -> Din2)
+
+Microswitch is conected with a GND pulldown resistor to Attiny_Pin1
+
+##3D printed parts
+![image2](./images/IMG_6424.JPG)
+I used a [Big button](https://www.thingiverse.com/thing:379920) model from [rmd6502](https://www.thingiverse.com/rmd6502/about) thingiverse user whit some modifications at cap button file adding 3D UNDO text over it.
+
+The file is [here](pulsador.stl) in stl format.
+
+You can mount button with a rubbish spring or without it. I had one in my trash components box :)
+
+##Software
+Only need to program attiny with your Arduino environment. 
+
+Don't forget:
+
+* [Adafruit Neopixel Library](https://github.com/adafruit/Adafruit_NeoPixel)
+* [Adafruit Trinket Keyboard Library](
+https://github.com/adafruit/Adafruit-Trinket-USB/tree/master/TrinketKeyboard)
+* Uncomment line (about 72-75) to use Undo Button on MAC or Windows-PC (COMMAND+Z or CONTROL+Z)
+* _interval definition is the time interval without pulsations to shift a level from red to green. By default is 60 seconds.
+
+![image3](./images/IMG_6555.JPG)
+Default color when you push Undo Button is purple **RGB(250,0,250)**
+![image4](./images/IMG_6557.JPG)
+If you put button in red color maybe you are not paying enough attention to what you do :p
+![image5](./images/IMG_6556.JPG)
+
+
+ENJOY IT...
+[![Youtube](./images/video.png)](https://www.youtube.com/watch?v=fLMrjbFxvYY)
+
+2020 Alfredo Prado
+
+@radikalbytes
--- Undo_button.ino
+++ Undo_button.ino
@@ -1,10 +1,28 @@
+/*
+Copyright 2020 Alfredo Prado Vega
+@radikalbytes
+http://www.radikalbytes.com
+This work is licensed under the Creative Commons Attribution-ShareAlike 3.0
+Unported License. To view a copy of this license, visit
+http://creativecommons.org/licenses/by-sa/3.0/ or send a letter to
+Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+
+Neopixel library and Trinket keyboard library are from adafruit.com and have their 
+own license. Please check it at adafruit.com
+https://github.com/adafruit/Adafruit-Trinket-USB/tree/master/TrinketKeyboard
+https://github.com/adafruit/Adafruit_NeoPixel
+
+*/
+
+
+// Adafruit Neopixel Library
 #include <Adafruit_NeoPixel.h>
 // Which pin on the Arduino is connected to the NeoPixels?
 #define PIN            0
 
-// How many NeoPixels are attached to the Arduino?
+// How many NeoPixels are attached to the circuit?
 #define NUMPIXELS      2
-
+// Interval in ms to down button color
 #define  _interval  60000
 
 // When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
@@ -12,15 +30,21 @@
 // example for more information on possible values.
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_RGB + NEO_KHZ800);
 
+// Adafruit Trinket Keyboard library
 #include <TrinketKeyboard.h>
 
+// Pin button
 #define buttonPin 1
 
+// Functions definition
 void put_color(int _r, int _g, int _b);
 
+// Button RGB values
 int __r = 0, __g = 250, __b = 0;
+// Delay aux
 unsigned long time1 = 0,time2 = 0;
 
+// Setup routine
 void setup()
 {
   // initialize the pushbutton pin as an input:
@@ -29,7 +53,7 @@ void setup()
   // start USB stuff
   TrinketKeyboard.begin();
   pixels.begin(); // This initializes the NeoPixel library.
-
+  // Put initial color (green)
   put_color(__r, __g, __b);
 
   time1 = millis();
@@ -43,23 +67,33 @@ void loop()
   // if it is not, then the computer may think that the device
   // has stopped working, and give errors
 
-  if (digitalRead(buttonPin) == HIGH)
+  if (digitalRead(buttonPin) == HIGH)  // button pushed
   {
     TrinketKeyboard.pressKey(KEYCODE_MOD_LEFT_GUI, KEYCODE_Z);
-    // this should type a CTRL+Z
+    // this should type a COMMAND+Z for MAC
+    // TrinketKeyboard.pressKey(KEYCODE_MOD_LEFT_CONTROL, KEYCODE_Z);
+    // this should type a CONTROLL+Z for Windows
     TrinketKeyboard.pressKey(0, 0);
     // this releases the key
-    put_color(250,0,250); 
+    
+    put_color(250,0,250); // put color purple 
+    // change next step from green to red
     if (__g > 0) {
         __r = __r + 25;
         __g = __g - 25;
     }
+
+    // Delay 200ms with pollig each 5 ms
     for (int _pollDelay = 0; _pollDelay < 40; _pollDelay++){
         delay(5);
         TrinketKeyboard.poll(); 
     }
+    // Change button color
     put_color(__r, __g, __b); 
   }
+
+  // If button not pushed in an interval of time, down one level
+  // from red to green
   time2 = millis(); 
   if (time2 > (time1 + _interval)){
     time1 = millis();
@@ -72,6 +106,7 @@ void loop()
   }
 }
 
+// Send color to neopixels
 void put_color(int _r, int _g, int _b){
   for(int i=0;i<NUMPIXELS;i++){
     // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
